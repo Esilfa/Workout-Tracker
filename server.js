be785e53258015d3ffc2ddb08d03083fd23bd511
Estefany Silfa <esilfa3@outlook.com>
2022-02-06 01:03:28 -0500

    update server

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const path = require("path");
 const apiRoutes = require('./routes/api-routes');
 const viewRoutes = require('./routes/html-routes');
 
+// mongodb+srv://esilfa:Codingpass1!@cluster0.pfval.mongodb.net/myFirstDatabase?retryWrites=true&w=majority,
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useUnifiedTopology: true });
 
 app.use(express.urlencoded({ extended: true }));
